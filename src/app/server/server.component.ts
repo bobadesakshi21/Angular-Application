4a6eb62cb2ab1c324af95155319bb076820101df
Sakshi Bobade <sbobade@rocketsoftware.com>
2021-07-24 15:18:33 +0530

    Event Handling 2

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -8,9 +8,14 @@ export class ServerComponent {
   
   allowNewServer = true;
   serverCreationStatus = 'No server was created';
+  serverName = '';
 
   onCreateServer() {
     this.serverCreationStatus = 'Server was created';
     console.log('Server Created')
+  }
+
+  onUpdateServerName(event: Event) {
+    this.serverName = (<HTMLInputElement>event.target).value;
   }
 }
